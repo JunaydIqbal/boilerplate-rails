c4bba5731e80ee11ba6836a2c5a50c77585190c8
Moiz Ali <amoiz4142@gmail.com>
2022-06-06 19:19:13 +0500

    Add gem simplecov and config

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails', '~> 5.0.0'
   gem 'shoulda-matchers', '~> 5.0'
+  gem 'simplecov', require: false
   # gem 'rubocop', require: false
   # gem 'rubocop-rails', require: false
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,7 @@
+# Load and launch SimpleCov at the very top to pick files and their coverage
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
