f10f9b2127d889879fb2b5bee5233cec46e810df
Moiz Ali <amoiz4142@gmail.com>
2022-06-10 18:33:06 +0500

    Add loginUser mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     # User fields
     field :register_user, mutation: Mutations::Users::RegisterUser
+    field :login_user, mutation: Mutations::Users::LoginUser
     # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
--- spec/graphql/types/mutation_type_spec.rb
+++ spec/graphql/types/mutation_type_spec.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
+require 'rails_helper'
 
 RSpec.describe Types::MutationType do
+  let(:fields_mapper) do
+    {
+      'registerUser' => Mutations::Users::RegisterUser,
+      'loginUser' => Mutations::Users::LoginUser
+    }
+  end
   
   context "Registered fields" do
-    it "should check inclusion of #registerUser" do
-      expect(described_class.fields.has_key?("registerUser")).to be(true)
-      expect(described_class.fields["registerUser"].mutation).to eq(Mutations::Users::RegisterUser)
+    it "should check inclusion of fields" do
+      fields_mapper.each do |field, klass|
+        expect(described_class.fields.has_key?(field)).to be(true)
+        expect(described_class.fields[field].mutation).to eq(klass)
+      end
     end
   end
 
