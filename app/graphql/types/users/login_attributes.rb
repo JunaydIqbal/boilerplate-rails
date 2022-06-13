37f7b80922545a62cfc6da4787d8e437cb53ed42
Moiz Ali <amoiz4142@gmail.com>
2022-06-13 18:50:56 +0500

    Add rememberMe with specs

--- app/graphql/types/users/login_attributes.rb
+++ app/graphql/types/users/login_attributes.rb
@@ -3,6 +3,7 @@ module Types
     class LoginAttributes < Types::BaseInputObject
       argument :email, String
       argument :password, String, required: true
+      argument :remember_me, Boolean, required: true
     end
   end
 end
--- spec/graphql/types/users/login_attributes_spec.rb
+++ spec/graphql/types/users/login_attributes_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Types::Users::LoginAttributes do
   context "Arguments" do
     it "should validate existence of attrs and their types" do
-      %w(email password).each do |attr|
+      %w(email password rememberMe).each do |attr|
         expect(described_class.arguments).to include(attr)
         expect(described_class.arguments[attr].type.class).to eq(GraphQL::Schema::NonNull)
-        expect(described_class.arguments[attr].type.of_type).to eq(GraphQL::Types::String)  
+        expect(described_class.arguments[attr].type.of_type).to eq(
+          attr == 'rememberMe' ? GraphQL::Types::Boolean : GraphQL::Types::String
+        )
       end
     end
   end
