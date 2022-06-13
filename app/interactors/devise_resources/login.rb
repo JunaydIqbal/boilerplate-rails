10e10550548690d8f34181a74cc8a1191bbe8dfc
Moiz Ali <amoiz4142@gmail.com>
2022-06-13 17:43:21 +0500

    Add login interactor with specs

---
+++ app/interactors/devise_resources/login.rb
@@ -0,0 +1,47 @@
+module DeviseResources
+  class Login < BaseInteractor
+
+    def call
+      @resource = grab_resource
+      # check_for_account_access if suspension_check_needed?
+      authenticate!
+    rescue => e
+      context.fail!(error: e.message)
+    end
+
+    private
+
+      def grab_resource
+        # Uncomment and adjust the code below for multiple devise models
+        # @resource = context.klass == "Customer" ? 
+        #   Customer.find_by(email: context.email) :
+        #   User.find_by(email: context.email)
+        User.find_by(email: context.resource_params[:email])
+      end
+
+      # def suspension_check_needed?
+      #   @resource.present? && @resource.is_a?(User)
+      # end
+
+      # def check_for_account_access
+      #   if @resource.revoke_access
+      #     raise 'Account Suspended. Please contact admin to get it enabled!'
+      #   end
+      # end
+
+      def authenticate!
+        if @resource && @resource.valid_password?(context.resource_params[:password])
+          context.resource = @resource
+          @token_result = DeviseResources::GenerateToken.call(resource: @resource, remember_me: context.remember_me)
+          if @token_result.success?
+            context.token = @token_result.token
+          else
+            context.fail!(error: @token_result.error)
+          end
+        else
+          raise 'Invalid Credentials'
+        end
+      end
+      
+  end
+end
