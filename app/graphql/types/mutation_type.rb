d1d109503ec4b885b47ffa09e0d8693dbea2e6ff
Moiz Ali <amoiz4142@gmail.com>
2022-06-14 17:56:30 +0500

    Add updateUser with specs.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     # User fields
     field :register_user, mutation: Mutations::Users::RegisterUser
     field :login_user, mutation: Mutations::Users::LoginUser
+    field :update_user, mutation: Mutations::Users::UpdateUser
     # TODO: remove me
     # field :test_field, String, null: false,
     #   description: "An example field added by the generator"
