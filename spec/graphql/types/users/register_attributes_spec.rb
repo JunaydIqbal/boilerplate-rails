c6aaf146fa86f5f2c0287db05c3c8ee9a6511190
Moiz Ali <amoiz4142@gmail.com>
2022-06-17 17:22:07 +0500

    Add base attributes specs

--- spec/graphql/types/users/register_attributes_spec.rb
+++ spec/graphql/types/users/register_attributes_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Types::Users::RegisterAttributes do
-  
-  let(:email_regex) { /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ }
-
   context "Config" do
     it "should check for parent class" do
       expect(described_class.superclass).to eql(Types::Users::BaseAttributes) 
@@ -11,7 +8,6 @@ RSpec.describe Types::Users::RegisterAttributes do
   end
 
   context "Arguments" do
-
     it "should validate existence of attrs and their types" do
       # Add more attributes inside collection to validate!
       %w(rememberMe).each do |attr|
@@ -23,12 +19,6 @@ RSpec.describe Types::Users::RegisterAttributes do
           # attr == "rememberMe" ? GraphQL::Types::Boolean : GraphQL::Types::String
         )
       end
-    end
-    
-    
-    it "should validate format of #email" do
-      expect(described_class.arguments["email"].validators).to include(GraphQL::Schema::Validator::FormatValidator)
-      expect(described_class.arguments["email"].validators.first.instance_variable_get(:@with_pattern)).to eq(email_regex)
     end
   end
 
