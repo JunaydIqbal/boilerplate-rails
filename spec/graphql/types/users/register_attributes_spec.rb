9b5811a1e8eb2e8e6e31393df00891746f155018
Moiz Ali <amoiz4142@gmail.com>
2022-06-17 17:15:11 +0500

    Adjust register attributes spec as arguments are moved in superclass

--- spec/graphql/types/users/register_attributes_spec.rb
+++ spec/graphql/types/users/register_attributes_spec.rb
@@ -4,14 +4,23 @@ RSpec.describe Types::Users::RegisterAttributes do
   
   let(:email_regex) { /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ }
 
+  context "Config" do
+    it "should check for parent class" do
+      expect(described_class.superclass).to eql(Types::Users::BaseAttributes) 
+    end
+  end
+
   context "Arguments" do
 
     it "should validate existence of attrs and their types" do
-      %w(email password passwordConfirmation rememberMe).each do |attr|
+      # Add more attributes inside collection to validate!
+      %w(rememberMe).each do |attr|
         expect(described_class.arguments).to include(attr)
         expect(described_class.arguments[attr].type.class).to eq(GraphQL::Schema::NonNull)
         expect(described_class.arguments[attr].type.of_type).to eq(
-          attr == "rememberMe" ? GraphQL::Types::Boolean : GraphQL::Types::String
+          GraphQL::Types::Boolean
+          # Adjust this logic according to type of argument
+          # attr == "rememberMe" ? GraphQL::Types::Boolean : GraphQL::Types::String
         )
       end
     end
