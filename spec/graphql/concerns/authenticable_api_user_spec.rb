4a0c0369a3defb59f39efb6aa7c01980fc327723
Moiz Ali <amoiz4142@gmail.com>
2022-06-17 20:32:14 +0500

    Add Authenticable API User module with specs

---
+++ spec/graphql/concerns/authenticable_api_user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+# Below class is used to test methods of AuthenticableApiUser module
+require './spec/support/resources/order_generator'
+
+RSpec.describe AuthenticableApiUser do
+  let(:user) { create(:user) }
+  let(:query) { 'mutation { orderGenerator( number: 1 ) { number } }' }
+  let(:result) { GraphqlBoilerplateSchema.execute(query) }
+  let(:result_1) { GraphqlBoilerplateSchema.execute(query, context: { current_user: user }) }
+
+  context "Methods" do
+    it "should check #ready?" do
+      expect(result["errors"][0]["message"]).to eql("Unauthorized error")
+    end
+
+    it "should check #ready? when #current_user is present" do
+      expect(result_1.dig('data', 'orderGenerator', 'number')).to eq(1)
+    end
+
+    it "should check #execution_error" do
+      expect(Order.new.send(:unauthorized_error)).to be_instance_of(GraphQL::ExecutionError)
+    end
+  end
+end
