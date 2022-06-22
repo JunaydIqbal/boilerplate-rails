a01695587af535306bb9e4720d4b53d384c3d46c
Moiz Ali <amoiz4142@gmail.com>
2022-06-22 18:43:22 +0500

    Send devise notifications through queue backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :invitable
 
   enum role: %i[admin user]
 
@@ -19,5 +19,10 @@ class User < ApplicationRecord
     else
       all
     end
+  end
+
+  # Override devise notification system to send emails through queue backend
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
   end
 end
