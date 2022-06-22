5a23352e12255734f59b49f03b5149ce56a424a1
Moiz Ali <amoiz4142@gmail.com>
2022-06-22 18:42:48 +0500

    Add root_url and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'home#index'
   post "/graphql", to: "graphql#execute"
+  
+  devise_for :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
