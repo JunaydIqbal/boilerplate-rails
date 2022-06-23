e708cda195bebffcaa36bc9680cc3136c2956ddb
Moiz Ali <amoiz4142@gmail.com>
2022-06-23 18:32:01 +0500

    Comment out LetterOpener path temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   
   devise_for :users
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
