150066488d83d1bf416939436491bb6dff246f46
Moiz Ali <amoiz4142@gmail.com>
2023-06-27 22:35:07 +0500

    Define dummy resources inside authenticable_api_user_spec

--- spec/graphql/concerns/authenticable_api_user_spec.rb
+++ spec/graphql/concerns/authenticable_api_user_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 # Below class is used to test methods of AuthenticableApiUser module
-require './spec/support/resources/order_generator'
+# require './spec/support/resources/order_generator'
+# A dummy class to test #execution_error
+class Order
+  include AuthenticableApiUser
+end
+
+# A dummy class to test GraphQL processes ..
+class OrderGenerator < GraphQL::Schema::Mutation
+  include AuthenticableApiUser
+
+  argument :number, Int
+
+  field :errors, [String]
+  field :number, Int
+
+  def resolve(number:)
+    { number: number }
+  end
+end
+
+# Register new mutation class inside MutationType
+module Types
+  class MutationType < Types::BaseObject
+    field :order_generator, mutation: OrderGenerator
+  end
+end
 
 RSpec.describe AuthenticableApiUser do
   let(:user) { create(:user) }
