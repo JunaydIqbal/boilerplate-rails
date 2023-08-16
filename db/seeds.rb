dba7853462f197386cdfd383617d8652d070dfe0
Moiz Ali <amoiz4142@gmail.com>
2023-08-16 17:30:16 +0500

    Add seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+admin = User.create(
+  full_name: "Admin 1", 
+  email: "admin@test.com",
+  password: "admin123",
+  password_confirmation: "admin123", 
+  phone_no: "+12334432493", 
+  role: "admin"
+)
+
+puts "*** Admin with email: #{admin.email} created! ***"; puts
+
+user = User.create(
+  full_name: "User 1", 
+  email: "user@test.com",
+  password: "admin123",
+  password_confirmation: "admin123", 
+  phone_no: "+12334432493", 
+  role: "user"
+)
+
+puts "*** User with email: #{user.email} created! ***"
