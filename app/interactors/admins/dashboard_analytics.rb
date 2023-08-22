648502106038bb7cec99cd137084583a9bd17147
Moiz Ali <amoiz4142@gmail.com>
2023-08-22 17:00:34 +0500

    Add AdminDashboardAnalytics interactor

---
+++ app/interactors/admins/dashboard_analytics.rb
@@ -0,0 +1,35 @@
+module Admins
+  class DashboardAnalytics < BaseInteractor
+    def call
+      build_analytics_data
+    rescue => e
+      context.fail!(error: e.message)      
+    end
+
+    private
+
+    def build_analytics_data
+      context.new_users_count = new_users_count
+      context.users_count = users_count
+      context.active_users_count = active_users_count
+      context.revoked_users_count = revoked_users_count
+    end
+
+    def new_users_count
+      # Users created in last 30 days
+      User.active.where(created_at: (30.days.ago..Date.today)).count
+    end
+
+    def users_count
+      User.count
+    end
+
+    def active_users_count
+      User.active.where("last_sign_in_at > ?", 7.days.ago.to_datetime).count
+    end
+
+    def revoked_users_count
+      User.where(revoke_access: true).count
+    end
+  end
+end
